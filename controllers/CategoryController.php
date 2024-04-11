fe48986d9accc79e434883bf6172823c5cbc28a3
newhask777 <newhaskellisp@gmail.com>
2024-04-11 14:37:26 +0300

    search, simple search by products

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -11,28 +11,37 @@ class CategoryController extends AppController
 {
     public function actionView($id)
     {
+
         $category = Category::findOne($id);
-        if(empty($category))
-        {
-            throw new NotFoundHttpException('Такой категории нет');
+        if(empty($category)){
+            throw new NotFoundHttpException('Такой категории нет...');
         }
 
-        $this->setMeta(
-            "{$category->title} :: " . \Yii::$app->name,
-            $category->keywords,
-            $category->description
-        );
+        $this->setMeta("{$category->title} :: " . \Yii::$app->name, $category->keywords, $category->description);
 
-//        $products = Product::find()->where(['category_id'=>$id])->all();
+        //$products = Product::find()->where(['category_id' => $id])->all();
 
-        $query = Product::find()->where(['category_id'=>$id]);
-        $pages = new Pagination([
-            'totalCount'=>$query->count(),
-            'pageSize'=>4
-        ]);
+        $query = Product::find()->where(['category_id' => $id]);
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 4, 'forcePageParam' => false, 'pageSizeParam' => false]);
         $products = $query->offset($pages->offset)->limit($pages->limit)->all();
 
-//        dd($products);
         return $this->render('view', compact('products', 'category', 'pages'));
     }
+
+    public function actionSearch()
+    {
+        $q = trim(\Yii::$app->request->get('q'));
+
+        $this->setMeta("Поиск: {$q} :: " . \Yii::$app->name);
+        if(!$q){
+            return $this->render('search');
+        }
+
+        $query = Product::find()->where(['like', 'title', $q]);
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 4, 'forcePageParam' => false, 'pageSizeParam' => false]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
+
+        return $this->render('search', compact('products', 'pages', 'q'));
+    }
+
 }
