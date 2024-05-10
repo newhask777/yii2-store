2b91262d4d20cee29bcc164e003862cf3c836204
newhask777 <newhaskellisp@gmail.com>
2024-05-10 18:07:22 +0300

    checkout

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -38,7 +38,10 @@ class CartController extends AppController
         $session->open();
         $cart = new Cart();
         $cart->recalc($id);
-        return $this->renderPartial('cart-modal', compact('session'));
+        if(\Yii::$app->request->isAjax){
+            return $this->renderPartial('cart-modal', compact('session'));
+        }
+        return $this->redirect(\Yii::$app->request->referrer);
     }
 
     public function actionClear()
@@ -51,10 +54,11 @@ class CartController extends AppController
         return $this->renderPartial('cart-modal', compact('session'));
     }
 
-    public function actionView()
+    public function actionCheckout()
     {
         $this->setMeta("Оформление заказа :: " . \Yii::$app->name);
-        return $this->render('view');
+        $session = \Yii::$app->session;
+        return $this->render('checkout', compact('session'));
     }
 
 }
--- web/js/main.js
+++ web/js/main.js
@@ -172,6 +172,16 @@ function clearCart() {
 
 /* Cart */
 
+// $('.value-plus').on('click', function(){
+//     var divUpd = $(this).parent().find('.value'), newVal = parseInt(divUpd.text(), 10)+1;
+//     divUpd.text(newVal);
+// });
+//
+// $('.value-minus').on('click', function(){
+//     var divUpd = $(this).parent().find('.value'), newVal = parseInt(divUpd.text(), 10)-1;
+//     if(newVal>=1) divUpd.text(newVal);
+// });
+
 
 
 
