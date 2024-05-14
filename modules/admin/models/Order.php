b123ad994fec5fbad699d0f4726a7841ef723443
newhask777 <newhaskellisp@gmail.com>
2024-05-14 14:49:51 +0300

    admin orders gii generate

--- modules/admin/models/Order.php
+++ modules/admin/models/Order.php
@@ -3,6 +3,9 @@
 namespace app\modules\admin\models;
 
 use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "orders".
@@ -19,7 +22,7 @@ use Yii;
  * @property string $address
  * @property string|null $note
  */
-class Order extends \yii\db\ActiveRecord
+class Order extends ActiveRecord
 {
     /**
      * {@inheritdoc}
@@ -27,6 +30,21 @@ class Order extends \yii\db\ActiveRecord
     public static function tableName()
     {
         return 'orders';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::class,
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                // если вместо метки времени UNIX используется datetime:
+                'value' => new Expression('NOW()'),
+            ],
+        ];
     }
 
     /**
@@ -51,16 +69,16 @@ class Order extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'created_at' => 'Created At',
-            'updated_at' => 'Updated At',
-            'qty' => 'Qty',
-            'total' => 'Total',
-            'status' => 'Status',
-            'name' => 'Name',
-            'email' => 'Email',
-            'phone' => 'Phone',
-            'address' => 'Address',
-            'note' => 'Note',
+            'created_at' => 'Добавлено',
+            'updated_at' => 'Обнавлено',
+            'qty' => 'Кол-во',
+            'total' => 'Всего',
+            'status' => 'Статус',
+            'name' => 'Название',
+            'email' => 'Почта',
+            'phone' => 'Телефон',
+            'address' => 'Адрес',
+            'note' => 'Заметка',
         ];
     }
 }
---
+++ modules/admin/views/order/index.php
@@ -0,0 +1,64 @@
+<?php
+
+use app\modules\admin\models\Order;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Список заказов';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <?= Html::a('Создать заказ', ['create'], ['class' => 'btn btn-success']) ?>
+            </div>
+
+            <div class="box-body">
+                <div class="order-index">
+
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
+
+                            'id',
+                            //'created_at',
+                            [
+                                'attribute' => 'created_at',
+                                'format' => ['datetime', 'php:d M Y H:i']
+                            ],
+                            //'updated_at',
+                            'qty',
+                            'total',
+                            'status',
+                            'name',
+                            'email:email',
+                            'phone',
+                            'address',
+                            //'note:ntext',
+                            [
+                                'class' => ActionColumn::class,
+                                'header' => 'Действие',
+                                'urlCreator' => function ($action, Order $model, $key, $index, $column) {
+                                    return Url::toRoute([$action, 'id' => $model->id]);
+                                }
+                            ],
+                        ],
+                    ]); ?>
+
+
+                </div>
+            </div>
+
+
+    </div>
+</div>
+
+
